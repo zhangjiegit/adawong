30c4487c90e0eb82e72c108732ef136ac98e5e78
phpdev <phpdev@sina.cn>
2015-05-05 14:06:44 +0800

    devel

--- system/classes/Ada/Utf8.php
+++ system/classes/Ada/Utf8.php
@@ -6,5 +6,16 @@
 * @author	cyhy
 */
 abstract class Ada_Utf8 {
-
+	
+	//ascii字符编码范围
+	const ASCII = '[\x00-\x7f]';
+	
+	/**
+	* 判断字符是否为ascii字符
+	* @param String $string	输入字符
+	* @return Bool
+	*/
+	public static function isAscii($string) {
+		return preg_match('/^'.self::ASCII.'$/', $string);
+	}
 }
