534f647d9c4d1dfcb082df7fbf51ee2c1554fa13
phpdev <phpdev@sina.cn>
2015-05-05 13:57:53 +0800

    devel

---
+++ system/classes/Ada/View.php
@@ -0,0 +1,10 @@
+<?php if (!defined('ADAPATH')) die ('Access failure');
+/**
+* 视图具体实现类
+* @package	AdaWong
+* @category	Base
+* @author	cyhy
+*/
+abstract class Ada_View {
+	
+}
---
+++ system/classes/View.php
@@ -0,0 +1,10 @@
+<?php if (!defined('ADAPATH')) die ('Access failure');
+/**
+* 视图类
+* @package	AdaWong
+* @category	Base
+* @author	cyhy
+*/
+class View extends Ada_View {
+
+}
