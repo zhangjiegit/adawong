01b17b59fb910155532b385324960597422b3f8e
phpdev <phpdev@sina.cn>
2015-05-05 13:13:44 +0800

    devel

--- index.php
+++ index.php
@@ -5,4 +5,3 @@ define('ADAPATH', dirname(__FILE__).DS.'system'.DS);
 include	ADAPATH.DS.'classes'.DS.'Ada'.DS.'Wong.php';
 spl_autoload_register(array('Ada_Wong','autoload'));
 echo request::factory()->method()->execute();
-?>
