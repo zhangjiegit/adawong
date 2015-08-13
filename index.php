a9e7c8ae417adf22dc57f1c78a4ba350042a24c1
zhangjie <zhangjiecyhy@sian.com>
2015-08-13 09:31:56 +0800

    test

--- index.php
+++ index.php
@@ -6,3 +6,5 @@ include	ADAPATH.DS.'classes'.DS.'Ada'.DS.'Wong.php';
 spl_autoload_register(array('Ada_Wong','autoload'));
 header('content-type:text/html;charset="utf-8"');
 echo Request::factory()->execute();
+echo time();
+echo date();
